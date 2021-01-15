f7e9172f4c676c80f3db7d2385c44f782344c0b9
GadoliniumPotassium <gdkh514@gmail.com>
2021-01-15 17:38:23 -0500

    introduction of a git ignore and completion of the data puller and processor

--- src/data_puller_and_processor.py
+++ src/data_puller_and_processor.py
@@ -12,27 +12,28 @@ moveList=txtFile.readlines()
 
 def get_all_moves(site):
     site_data=requests.get(site)
-    soup=BeautifulSoup(site_data.text,'html.parser')
+    soup=BeautifulSoup(site_data.text,'lxml')
     all_moves=soup.find_all("div",{"class":"movecontainer"})
     return all_moves
 
 def filter_data(data):
-    soup=BeautifulSoup(data.text,'html.parser')
     data_string=""
-    movename=soup.find_all("div",{"class":"movename"})[0].text
-    startup=soup.find_all("div",{"class":"startup"})[0].text
-    total_frames=soup.find_all("div",{"class":"totalframes"})[0].text
-    landing_lag=soup.find_all("div",{"class":"landinglag"})[0].text
-    base_damage=soup.find_all("div",{"class":"basedamage"})[0].text
-    notes=soup.find_all("div",{"class":"notes"})[0].text
-    shield_lag=find_all("div",{"class":"shieldlag"})[0].text
-    shield_stun=find_all("div",{"class":"shieldstun"})[0].text    
-    on_shield=find_all("div",{"class":"advantage"})[0].text
-    active_frame=find_all("div",{"class":"activeframes"})[0].text
-    whichhitbox=find_all("div",{"class","whichhitbox"})[0].text
+    soup=BeautifulSoup(str(data),'lxml')
+    movename=soup.find_all("div",{"class" : "movename"})[0].text.strip()
+    startup=soup.find_all("div",{"class":"startup"})[0].text.strip()
+    total_frames=soup.find_all("div",{"class":"totalframes"})[0].text.strip()
+    landing_lag=soup.find_all("div",{"class":"landinglag"})[0].text.strip()
+    base_damage=soup.find_all("div",{"class":"basedamage"})[0].text.strip()
+    notes=soup.find_all("div",{"class":"notes"})[0].text.strip()
+    shield_lag=soup.find_all("div",{"class":"shieldlag"})[0].text.strip()
+    shield_stun=soup.find_all("div",{"class":"shieldstun"})[0].text.strip()  
+    on_shield=soup.find_all("div",{"class":"advantage"})[0].text.strip()
+    active_frame=soup.find_all("div",{"class":"activeframes"})[0].text.strip()
+    whichhitbox=soup.find_all("div",{"class","whichhitbox"})[0].text.strip()
     
     #now we are going to fill the data string with all the information and then return it
-    data_string = "this move is "+ movename + "\nit starts up on frame "+startup + "\nit lasts for "+total_frames+" frames with " + active_frame + "being the active frames\n"+ "it has "+landing_lag+" frames of landing lag\n"+ "it has base damage of "+base_damage+"\n"+ "it has " + shield_lag + "frames of shield lag and " + shield_stun " frames of shield stun\n" + "it is also " + on_shield + " frames on shield.\n"+ "important to note on this move is that " + notes+ "." 
+    data_string = "this move is "+ movename + "\nit starts up on frame(s) "+startup + "\nit lasts for "+total_frames+" frames with frame(s) " + active_frame + " being the active frame(s)\n"+ "it has "+landing_lag+" frames of landing lag\n"+ "it has base damage of "+base_damage+"\n"+ "it has " + shield_lag + "frames of shield lag and " + shield_stun + " frames of shield stun\n" + "it is also " + on_shield + " frames on shield.\n"+ "Important to note on this move is that " + notes+ "." + "special note:" + whichhitbox 
+    print(data_string)
     return data_string
 
 
@@ -41,20 +42,18 @@ def isolate_req_move_post_data(all_moves,req_move):
     for move in moveList:
         if req_move in str(move):
             a=move.split(":")
-            print(a)
             req_move=a[0]
-            print (req_move)
             break
     
     move=""
+    move_div=""
     for move in all_moves:
         if str(req_move).lower() in str(move).lower():
             move_exists=True
+            move_div=move
             break
-    
-    print(move)   
+
     if move_exists==False: 
         return "Sorry baby, I don't know what you're talking about."
-    
-    return filer_data(move):
 
+    return filter_data(move_div) 
